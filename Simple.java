1b673c55070a14890f5014023823d21383545431
vaibhavpawar22987 <143528773+vaibhavpawar22987@users.noreply.github.com>
2023-09-04 20:15:14 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple {
-	public static void main (string[] args) {
-		system.out.println("hello edureka");
+	public static void main (String[] args) {
+		System.out.println("hello edureka");
 	}
 }
